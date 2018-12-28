0b27ca23fc3340e198ab4d883acccd43492d1112
gngdb <gavingray1729@gmail.com>
2018-12-28 13:51:20 +0000

    thought this was comitted

--- gen_experiment_schedule.py
+++ gen_experiment_schedule.py
@@ -2,11 +2,12 @@ import json
 
 if __name__ == '__main__':
     experiments = []
-    for wd in ['-d', None]:
-        for cscale in range(2,32):
-            experiment = ["python", "dawn.py", "%i"%cscale]
-            if wd is not None:
-                experiment += [wd]
+    for rankscale in range(2,16):
+        for dimensions in range(2,16):
+            experiment = ["python", "dawn.py", "%i"%rankscale, "%i"%dimensions]
             experiments.append(experiment)
+    print("Number of experiments: ", len(experiments))
+    print("Time to run: %i minutes"%(len(experiments)*20))
+    print("Time to run on 8 GPUs: %i minutes"%(len(experiments)*20/8))
     with open("experiment_schedule.json", "w") as f:
         f.write(json.dumps(experiments))
